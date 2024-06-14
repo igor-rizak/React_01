ed4014496d549b7e6fe9d2d3eb921ce13ce33fb6
igor-rizak <igor.rizak@ukr.net>
2024-06-14 19:18:45 +0300

    first

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from '../src/components/user.json';
+import { Profile } from './components/Profile/Profile.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   </React.StrictMode>
 );
