ec3da7112aec9ffdc557296345fb21ff55aa2e80
igor-rizak <igor.rizak@ukr.net>
2024-06-14 20:30:32 +0300

    end 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import user from './Json/user.json';
+import { Profile } from './Profile/Profile.jsx';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Юлька ВРЄДІНКА!!!
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        Юлька ВРЄДІНКА!!! ДУУУУУУУУУУУУУУЖЕ ВЕЛИКА
+      </div>
+
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import css from './Statistics.module.css';
+import stats from '../Json/data.json';
+
+export const Statistics = ({ id, label, percentage, stats }) => {
+  return (
+    <section class="statistics">
+      <h2 class="title">Upload stats</h2>
+      <ul class="stat-list">
+        {stats.map(stat => (
+          <li className={css.item} id={id}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}</span>
+          </li>
+        ))}
+        {/*               
+        <li class="item">
+          <span class="label">.docx</span>
+          <span class="percentage">4%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp3</span>
+          <span class="percentage">14%</span>
+        </li>
+        <li class="item">
+          <span class="label">.pdf</span>
+          <span class="percentage">41%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp4</span>
+          <span class="percentage">12%</span>
+        </li> */}
+      </ul>
+    </section>
+  );
+};
